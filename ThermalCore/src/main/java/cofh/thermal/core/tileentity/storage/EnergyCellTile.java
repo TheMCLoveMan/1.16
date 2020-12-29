f36afee3af73f612d7f62a060d9ee9fe157f0464
King Lemming <kinglemming@gmail.com>
2020-12-29 04:17:38 -0800

    Creative Augment support.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- ThermalCore/src/main/java/cofh/thermal/core/tileentity/storage/EnergyCellTile.java
+++ ThermalCore/src/main/java/cofh/thermal/core/tileentity/storage/EnergyCellTile.java
@@ -65,7 +65,7 @@ public class EnergyCellTile extends CellTileBase implements ITickableTileEntity 
     @Override
     public int getLightValue() {
 
-        return levelTracker;
+        return Math.min(levelTracker, 8);
     }
 
     protected void transferRF() {
@@ -146,7 +146,11 @@ public class EnergyCellTile extends CellTileBase implements ITickableTileEntity 
                 markDirty();
             }
         }
-        curScale = energyStorage.getEnergyStored() > 0 ? 1 + Math.min((int) (energyStorage.getRatio() * 8), 7) : 0;
+        if (energyStorage.isCreative()) {
+            curScale = 9;
+        } else {
+            curScale = energyStorage.getEnergyStored() > 0 ? 1 + Math.min((int) (energyStorage.getRatio() * 8), 7) : 0;
+        }
         if (levelTracker != curScale) {
             levelTracker = curScale;
             if (send) {
